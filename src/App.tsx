77a16a26e00907b47533fe8cef9779b388df2028
Adarsh <itsadarsh33@gmail.com>
2025-10-23 22:51:51 +0530

    Lint error fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Routes, Route, Navigate } from 'react-router-dom';
 import { AppPageLayout } from './components/AppPageLayout';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
